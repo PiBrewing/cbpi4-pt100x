178a3cb55630bfdc36c31b5e11e6ed679e193b67
avollkopf <43980694+avollkopf@users.noreply.github.com>
2023-03-27 22:03:33 +0200

    bug fix

--- cbpi4-pt100x/__init__.py
+++ cbpi4-pt100x/__init__.py
@@ -170,7 +170,7 @@ class CustomSensor(CBPiSensor):
             await asyncio.sleep(self.Interval)
 
     async def logvalue(self,value=None):
-        if self.reducedfrequency == 0:
+        if self.reducedfrequency != 0:
             currentvalue=self.value if value is None else value
             now=time.time()            
             if self.kettle is not None:
