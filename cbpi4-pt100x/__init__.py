50e105c30119c556fa1ddec98427e0929c9e1b2e
avollkopf <alexander.vollkopf@gmail.com>
2025-02-26 19:03:24 +0100

    add D9 to D11 to helper lisr

--- cbpi4-pt100x/__init__.py
+++ cbpi4-pt100x/__init__.py
@@ -58,7 +58,7 @@ class CustomSensor(CBPiSensor):
         self.mosiPin = 10
         self.clkPin  = 11
 
-        self.pins = [board.D0, board.D1, board.D2, board.D3, board.D4, board.D5, board.D6, board.D7, board.D8, board.D12, board.D13, board.D14, board.D15, board.D16, board.D17, board.D18, board.D19, board.D20, board.D21, board.D22, board.D23, board.D24, board.D25, board.D26, board.D27]
+        self.pins = [board.D0, board.D1, board.D2, board.D3, board.D4, board.D5, board.D6, board.D7, board.D8, board.D9, board.D10, board.D11, board.D12, board.D13, board.D14, board.D15, board.D16, board.D17, board.D18, board.D19, board.D20, board.D21, board.D22, board.D23, board.D24, board.D25, board.D26, board.D27]
         self.csPin = int(self.props.get("csPin",17))
         spi = board.SPI()
         cs = digitalio.DigitalInOut(self.pins[self.csPin])
